0817eb81272db44af99f639a6f8d9103d596480b
Jonathan Xavier <jonathan.rodrigues@mv.com.br>
2024-10-16 14:26:51 -0300

    feat: Pegando valor do input

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,10 +1,25 @@
 import styles from './AddTask.module.css'
+import { SetStateAction, useState} from 'react'
+
+
+
 
 export function AddTask(){
+    const [inputValue, setInputValue] = useState('')
+
+    const handleChange = (event: { target: { value: SetStateAction<string> } }) => {
+        setInputValue(event.target.value)
+    }
+
+    const handleClick = () => {
+       console.log(inputValue) 
+    }
+
+ 
     return(
         <div className={styles.content}>
-            <input className={styles.input} type="text" placeholder='Adicione uma nova tarefa'/>
-            <button className={styles.addButton}>Criar +</button>
+            <input className={styles.input} value={inputValue} onChange={handleChange} type="text" placeholder='Adicione uma nova tarefa'/>
+            <button className={styles.addButton}  onClick={handleClick}>Criar +</button>
         </div>
     )
 }
